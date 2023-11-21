45ee8e1c1f39f5ee3ffca1d27a8b51bfe9e85ea2
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 16:50:14 +0100

    Add components to page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,27 @@
 import { useTranslations } from "next-intl";
-import { H1 } from "./components/Typography";
+import { H1, H2, TextRegular } from "./components/Typography";
 import MainContainer from "./components/containers/MainContainer";
+import TextContainer from "./components/containers/TextContainer";
+import ButtonStandard from "./components/Button/variants/ButtonStandard";
 
 export default function Home() {
   const t = useTranslations("Index");
-  return <MainContainer>{/* <H1>{t("title")}</H1> */}</MainContainer>;
+
+  return (
+    <MainContainer>
+      <TextContainer>
+        <H1>{t("title")}</H1>
+        <H2>{t("subtitle")}</H2>
+      </TextContainer>
+      <TextContainer>
+        <TextRegular>{t("message")}</TextRegular>
+      </TextContainer>
+      <div className="w-full h-fit flex flex-col gap-6 mt-10">
+        <ButtonStandard>{t("new_game_button_text")}</ButtonStandard>
+        <ButtonStandard background="secondLight">
+          {t("rules_button_text")}
+        </ButtonStandard>
+      </div>
+    </MainContainer>
+  );
 }
