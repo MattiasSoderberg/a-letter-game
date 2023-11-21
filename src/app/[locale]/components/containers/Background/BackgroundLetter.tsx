89b51cba58e98f41e58e6ab1043733c441efb2d8
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 16:52:21 +0100

    Tweak animation

--- src/app/[locale]/components/containers/Background/BackgroundLetter.tsx
+++ src/app/[locale]/components/containers/Background/BackgroundLetter.tsx
@@ -18,17 +18,18 @@ const BackgroundLetter = ({
   return (
     <motion.p
       animate={{
-        opacity: [0.6, 1, 0.6],
-        scale: [0.9, 1.1, 0.9],
+        opacity: [0.6, 1, 1, 0.6],
+        scale: [0.8, 1.2, 1.2, 0.8],
       }}
       transition={{
         delay: delay,
         repeat: Infinity,
         repeatDelay: 2 + delay,
-        duration: 2 + delay,
+        duration: 4 + delay,
         ease: "easeInOut",
+        times: [0, 0.4, 0.6, 1],
       }}
-      className={`text-4xl text-${color} absolute ${position}`}
+      className={`text-4xl text-${color} absolute ${position} md:text-5xl`}
     >
       {children}
     </motion.p>
