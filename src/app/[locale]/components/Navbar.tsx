2e233e71dca24e62638523a78ba4ae04bcd721c4
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 14:45:48 +0100

    Add navbar

---
+++ src/app/[locale]/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "./SVG/Logo";
+import Menu from "./SVG/Menu";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-navHeight flex justify-between bg-lightMain relative z-30 p-5">
+      <div>
+        <Logo />
+      </div>
+      <Menu />
+    </div>
+  );
+};
+
+export default Navbar;
