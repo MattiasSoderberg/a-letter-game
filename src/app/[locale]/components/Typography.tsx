6f0d901d17130f547a2fad9e07fe45c37716703d
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 16:50:52 +0100

    Add typographies

--- src/app/[locale]/components/Typography.tsx
+++ src/app/[locale]/components/Typography.tsx
@@ -17,3 +17,33 @@ export const H1 = ({ children }: Props) => {
     <h1 className={`${classes.size} ${globalStyles.color}`}>{children}</h1>
   );
 };
+
+export const H2 = ({ children }: Props) => {
+  const classes = {
+    size: "text-xl",
+  };
+  return (
+    <h2 className={`${classes.size} ${globalStyles.color}`}>{children}</h2>
+  );
+};
+
+export const TextLarge = ({ children }: Props) => {
+  const classes = {
+    size: "text-lg",
+  };
+  return <p className={`${classes.size} ${globalStyles.color}`}>{children}</p>;
+};
+
+export const TextRegular = ({ children }: Props) => {
+  const classes = {
+    size: "text-base",
+  };
+  return <p className={`${classes.size} ${globalStyles.color}`}>{children}</p>;
+};
+
+export const TextSmall = ({ children }: Props) => {
+  const classes = {
+    size: "text-sm",
+  };
+  return <p className={`${classes.size} ${globalStyles.color}`}>{children}</p>;
+};
