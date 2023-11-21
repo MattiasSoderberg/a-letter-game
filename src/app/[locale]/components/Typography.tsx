966ae85c066012c35576bf6d6d8d7c91700b77ed
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 14:46:07 +0100

    Add styles

--- src/app/[locale]/components/Typography.tsx
+++ src/app/[locale]/components/Typography.tsx
@@ -2,11 +2,18 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  color?: string;
 }
+
+const globalStyles = {
+  color: "text-darkMain",
+};
 
 export const H1 = ({ children }: Props) => {
   const classes = {
     size: "text-3xl",
   };
-  return <h1>{children}</h1>;
+  return (
+    <h1 className={`${classes.size} ${globalStyles.color}`}>{children}</h1>
+  );
 };
