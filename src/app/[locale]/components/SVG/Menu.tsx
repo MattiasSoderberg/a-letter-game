5b1234f978a827c2fe76c2b8518792fc5d66b08b
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 14:47:02 +0100

    Add basic menu component

---
+++ src/app/[locale]/components/SVG/Menu.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <svg viewBox="0 0 100 80" width={30} height={30}>
+      <rect width={100} height={12} />
+      <rect y={30} width={100} height={12} />
+      <rect y={60} width={100} height={12} />
+    </svg>
+  );
+};
+
+export default Menu;
