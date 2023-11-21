f35e465718004cdbe42eb82a5c40ab86dfbe5c60
Mattias Söderberg <mattes.email@gmail.com>
2023-11-21 16:50:38 +0100

    Add more padding for bigger screen

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Menu from "./SVG/Menu";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-navHeight flex justify-between bg-lightMain relative z-30 p-5">
+    <div className="w-full h-navHeight flex justify-between bg-lightMain relative z-30 p-5 md:px-10">
       <div>
         <Logo />
       </div>
