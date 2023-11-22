42623ef1cf90f7445e1a2bd83f12fe1f5ed76392
Mattias Söderberg <mattes.email@gmail.com>
2023-11-22 15:00:49 +0100

    Add menu button

--- src/app/[locale]/components/Navbar.tsx
+++ src/app/[locale]/components/Navbar.tsx
@@ -1,14 +1,20 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Logo from "./SVG/Logo";
-import Menu from "./SVG/Menu";
+import ButtonNaked from "./Button/variants/ButtonNaked";
+import ButtonMenu from "./Button/variants/ButtonMenu";
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const onClick = () => {
+    setIsActive(!isActive);
+  };
   return (
     <div className="w-full h-navHeight flex justify-between bg-lightMain relative z-30 p-5 md:px-10">
       <div>
         <Logo />
       </div>
-      <Menu />
+      <ButtonMenu />
     </div>
   );
 };
