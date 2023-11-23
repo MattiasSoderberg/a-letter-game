19467929658e007341defcac52537d6f73904276
Mattias Söderberg <mattes.email@gmail.com>
2023-11-23 12:30:33 +0100

    Add rules page

---
+++ src/app/[locale]/rules/page.tsx
@@ -0,0 +1,16 @@
+import { useTranslations } from "next-intl";
+import { H1 } from "../components/Typography";
+import MainContainer from "../components/containers/MainContainer";
+import TextContainer from "../components/containers/TextContainer";
+
+export default function Rules() {
+  const t = useTranslations("Rules");
+
+  return (
+    <MainContainer>
+      <TextContainer>
+        <H1>{t("title")}</H1>
+      </TextContainer>
+    </MainContainer>
+  );
+}
