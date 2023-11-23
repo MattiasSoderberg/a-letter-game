99da6068b325d57fbc72b861d76fe1568797bf36
Mattias Söderberg <mattes.email@gmail.com>
2023-11-23 12:30:23 +0100

    Add about page

---
+++ src/app/[locale]/about/page.tsx
@@ -0,0 +1,19 @@
+import { useTranslations } from "next-intl";
+import { H1 } from "../components/Typography";
+import MainContainer from "../components/containers/MainContainer";
+import TextContainer from "../components/containers/TextContainer";
+
+export default function About({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = useTranslations("About");
+  return (
+    <MainContainer>
+      <TextContainer>
+        <H1>{t("title")}</H1>
+      </TextContainer>
+    </MainContainer>
+  );
+}
