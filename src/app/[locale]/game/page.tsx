a669cdf3634e9328bfdd57ca470af35005e1f406
Mattias Söderberg <mattes.email@gmail.com>
2023-11-23 12:30:40 +0100

    Add game page

---
+++ src/app/[locale]/game/page.tsx
@@ -0,0 +1,13 @@
+import { H1 } from "../components/Typography";
+import MainContainer from "../components/containers/MainContainer";
+import TextContainer from "../components/containers/TextContainer";
+
+export default function Game() {
+  return (
+    <MainContainer>
+      <TextContainer>
+        <H1>Game</H1>
+      </TextContainer>
+    </MainContainer>
+  );
+}
