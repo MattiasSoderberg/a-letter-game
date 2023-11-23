25e1aa5f881b5ce4e598d0839add7b3f3f6aa2fc
Mattias Söderberg <mattes.email@gmail.com>
2023-11-23 12:32:30 +0100

    Responsive styles

--- src/app/[locale]/components/containers/MainContainer.tsx
+++ src/app/[locale]/components/containers/MainContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const MainContainer = ({ children }: Props) => {
   return (
-    <div className="w-full h-full flex flex-col items-center gap-6 px-5 py-8 relative z-20 md:p-20">
+    <div className="w-full h-full flex flex-col items-center gap-6 px-5 py-8 relative z-20 md:p-20 lg:px-60 xl:px-80 2xl:px-96">
       {children}
     </div>
   );
