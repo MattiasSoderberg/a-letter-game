d5df1d7fee7a20442e6230a2a2482e6b78cdff3c
Mattias Söderberg <mattes.email@gmail.com>
2023-11-24 13:28:26 +0100

    Fix issues with countdown

--- src/app/components/Game/Game.tsx
+++ src/app/components/Game/Game.tsx
@@ -1,22 +1,38 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useAppContext } from "@/context/AppContext";
 import { TextRegular } from "../Typography";
 import { useRouter } from "@/navigation";
 import PlayerCard from "./PlayerCard";
 import LetterGenerator from "./LetterGenerator";
 import { useTranslations } from "next-intl";
+import GameTimer from "./GameTimer";
 
 const Game = () => {
+  const [currentLetter, setCurrentLetter] = useState("?");
+  const [isRoundActive, setIsRoundActive] = useState(false);
   const { players } = useAppContext();
   const router = useRouter();
   const t = useTranslations("Utils");
+
+  const handleSetCurrentLetter = (letter: string) => {
+    setCurrentLetter(letter);
+    if (letter !== "?") {
+      setIsRoundActive(true);
+    }
+  };
+
+  const handleSetRoundIsActive = (active: boolean) => {
+    setIsRoundActive(active);
+  };
 
   useEffect(() => {
     if (players.length <= 0) {
       router.push("/game/new-game");
     }
   }, []);
+
+  console.log(currentLetter, isRoundActive);
 
   return (
     <div className="w-full h-full flex flex-col gap-10">
@@ -30,7 +46,15 @@ const Game = () => {
           />
         ))}
       </div>
-      <LetterGenerator alphabeth={t("alphabeth")} />
+      <GameTimer
+        isRoundActive={isRoundActive}
+        handleSetRoundIsActive={handleSetRoundIsActive}
+      />
+      <LetterGenerator
+        alphabeth={t("alphabeth")}
+        currentLetter={currentLetter}
+        handleSetCurrentLetter={handleSetCurrentLetter}
+      />
     </div>
   );
 };
