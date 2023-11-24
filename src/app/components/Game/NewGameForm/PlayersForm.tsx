64277b792100c3f7f782207bd4686fcdb9d4323f
Mattias Söderberg <mattes.email@gmail.com>
2023-11-24 08:43:00 +0100

    Add players form for new game

---
+++ src/app/components/Game/NewGameForm/PlayersForm.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Player } from "./NewGameForm";
+import Input from "../../Form/Input";
+import { TextLarge } from "../../Typography";
+import ButtonStandard from "../../Button/variants/ButtonStandard";
+
+interface Props {
+  players: Player[];
+  handleSetPlayers: (players: Player[]) => void;
+  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const PlayersForm = ({ players, handleSetPlayers, onSubmit }: Props) => {
+  const handleOnChange = (
+    index: number,
+    event: React.FormEvent<HTMLInputElement>
+  ) => {
+    const data = [...players];
+    data[index];
+    data[index]["name"] = event.currentTarget.value;
+    handleSetPlayers(data);
+  };
+
+  return (
+    <div className="w-full h-full flex flex-col gap-6 mb-6 overflow-hidden">
+      <form
+        className="w-full h-full flex flex-col gap-6 pb-10 overflow-y-auto"
+        onSubmit={(e) => onSubmit(e)}
+      >
+        {players.length > 0 &&
+          players.map((player, index) => (
+            <div
+              key={index}
+              className="w-full h-max flex flex-col gap-4 p-4 bg-textContainerBG rounded shadow-md shadow-secondLight"
+            >
+              <TextLarge>{`Spelare ${index + 1}`}</TextLarge>
+              <div className="flex flex-col gap-2">
+                <label htmlFor={`input${index}`}>Namn</label>
+                <Input
+                  name="name"
+                  value={player.name}
+                  onChange={handleOnChange}
+                  index={index}
+                />
+              </div>
+            </div>
+          ))}
+        <ButtonStandard type="submit">Starta</ButtonStandard>
+      </form>
+    </div>
+  );
+};
+
+export default PlayersForm;
