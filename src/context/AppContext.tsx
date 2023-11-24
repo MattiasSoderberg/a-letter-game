1aed726eaa0218919994eb618bd22ced1c72d07a
Mattias Söderberg <mattes.email@gmail.com>
2023-11-24 16:12:33 +0100

    Add game settings

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -10,12 +10,21 @@ type Player = {
   points: number;
 };
 
+type GameSettings = {
+  numberOfPlayers: number;
+  repeatingLetters: boolean;
+  numberOfRounds: number;
+  lengthOfRounds: number;
+};
+
 interface AppContextType {
   handleOnOpen: () => void;
   handleOnClose: () => void;
   isMenuOpen: boolean;
   players: Player[];
   handleSetPlayers: (players: Player[]) => void;
+  gameSettings: GameSettings;
+  handleSetGameSettings: (settings: GameSettings) => void;
 }
 
 export const AppContext = createContext<AppContextType | null>(null);
@@ -33,6 +42,12 @@ export const useAppContext = () => {
 export const AppContextProvider = ({ children }: Props) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [players, setPlayers] = useState<Player[]>([]);
+  const [gameSettings, setGameSettings] = useState<GameSettings>({
+    numberOfPlayers: 0,
+    repeatingLetters: true,
+    numberOfRounds: 10,
+    lengthOfRounds: 30,
+  });
 
   const handleOnOpen = () => {
     setIsMenuOpen(true);
@@ -46,12 +61,18 @@ export const AppContextProvider = ({ children }: Props) => {
     setPlayers(players);
   };
 
+  const handleSetGameSettings = (settings: GameSettings) => {
+    setGameSettings(settings);
+  };
+
   const value = {
     isMenuOpen,
     handleOnOpen,
     handleOnClose,
     handleSetPlayers,
     players,
+    handleSetGameSettings,
+    gameSettings,
   };
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
