27c3be60c17fe459cf162ba5611c0b4e273e4d69
Mattias Söderberg <mattes.email@gmail.com>
2023-11-24 16:08:25 +0100

    Add border padding prop

--- src/app/components/Button/variants/ButtonNaked.tsx
+++ src/app/components/Button/variants/ButtonNaked.tsx
@@ -3,14 +3,17 @@ import Button from "../Button";
 
 interface Props {
   children: React.ReactNode;
+  paddings?: string;
+  borders?: boolean;
   hovers?: string;
   onClick?: () => void;
 }
 
-const ButtonNaked = ({ hovers, ...rest }: Props) => {
+const ButtonNaked = ({ hovers, borders = false, ...rest }: Props) => {
   const classes = {
     background: "transparent",
     paddings: "",
+    borders: borders ? "border border-1 border-darkMain" : "",
     hovers: hovers,
   };
   return <Button {...classes} {...rest} />;
