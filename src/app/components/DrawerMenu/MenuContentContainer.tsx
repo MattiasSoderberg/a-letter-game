0975d0d81f39b51119b38b83b0ccd27d43836fbb
Mattias Söderberg <mattes.email@gmail.com>
2023-11-28 09:17:39 +0100

    Change some styling

--- src/app/components/DrawerMenu/MenuContentContainer.tsx
+++ src/app/components/DrawerMenu/MenuContentContainer.tsx
@@ -1,16 +1,16 @@
 "use client";
 import React from "react";
-import { motion } from "framer-motion";
-import { useAppContext } from "@/context/AppContext";
 import useMenu from "@/hooks/useMenu";
+import { motion } from "framer-motion";
 import { Link } from "@/navigation";
 import { TextRegular } from "../Typography";
 import LanguageSwitcher from "./LanguageSwitcher";
 import { useTranslations } from "next-intl";
+import { useAppContext } from "@/context/AppContext";
 
 const MenuContentContainer = () => {
-  const { isMenuOpen } = useAppContext();
   const { closeMenu } = useMenu();
+  const { isMenuOpen } = useAppContext();
   const t = useTranslations("Navigation");
 
   return (
@@ -18,29 +18,31 @@ const MenuContentContainer = () => {
       initial={{ x: "100%" }}
       animate={isMenuOpen ? { x: 0 } : { x: "100%" }}
       transition={{ type: "spring", stiffness: 130, damping: 20 }}
-      className="w-full h-full flex flex-col gap-10 bg-lightMain absolute z-50 p-10"
+      className={`w-full h-full absolute z-50`}
     >
-      <ul className="w-full flex flex-col items-start gap-8">
-        <li className="w-full flex flex-col gap-2">
-          <Link href="/about" onClick={closeMenu}>
-            <TextRegular>{t("about")}</TextRegular>
-          </Link>
-          <div className="w-full h-[1px] bg-firstDark" />
-        </li>
-        <li className="w-full flex flex-col gap-2">
-          <Link href="/rules" onClick={closeMenu}>
-            <TextRegular>{t("rules")}</TextRegular>
-          </Link>
-          <div className="w-full h-[1px] bg-firstDark" />
-        </li>
-        <li className="w-full flex flex-col gap-2">
-          <Link href="/game" onClick={closeMenu}>
-            <TextRegular>{t("play")}</TextRegular>
-          </Link>
-          <div className="w-full h-[1px] bg-firstDark" />
-        </li>
-      </ul>
-      <LanguageSwitcher />
+      <div className="w-full h-full flex flex-col gap-10 bg-lightMain p-10">
+        <ul className="w-full h-max flex flex-col items-start gap-8">
+          <li className="w-full flex flex-col gap-2">
+            <Link href="/about" onClick={closeMenu}>
+              <TextRegular>{t("about")}</TextRegular>
+            </Link>
+            <div className="w-full h-[1px] bg-firstDark" />
+          </li>
+          <li className="w-full flex flex-col gap-2">
+            <Link href="/rules" onClick={closeMenu}>
+              <TextRegular>{t("rules")}</TextRegular>
+            </Link>
+            <div className="w-full h-[1px] bg-firstDark" />
+          </li>
+          <li className="w-full flex flex-col gap-2">
+            <Link href="/game" onClick={closeMenu}>
+              <TextRegular>{t("play")}</TextRegular>
+            </Link>
+            <div className="w-full h-[1px] bg-firstDark" />
+          </li>
+        </ul>
+        <LanguageSwitcher />
+      </div>
     </motion.div>
   );
 };
