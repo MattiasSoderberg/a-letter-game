4288400ba27567c83f536bfa1a93521e7d35a053
Mattias Söderberg <mattes.email@gmail.com>
2023-11-28 12:02:19 +0100

    Wrap new game form with next intl provider

--- src/app/[locale]/game/new-game/page.tsx
+++ src/app/[locale]/game/new-game/page.tsx
@@ -2,16 +2,24 @@ import NewGameForm from "@/app/components/Game/NewGameForm/NewGameForm";
 import { H1 } from "@/app/components/Typography";
 import MainContainer from "@/app/components/containers/MainContainer";
 import TextContainer from "@/app/components/containers/TextContainer";
-import { useTranslations } from "next-intl";
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from "next-intl";
 
 export default function NewGame() {
   const t = useTranslations("Game.new_game");
+  const messages = useMessages();
+
   return (
     <MainContainer>
       <TextContainer>
         <H1>{t("title")}</H1>
       </TextContainer>
-      <NewGameForm />
+      <NextIntlClientProvider messages={messages}>
+        <NewGameForm />
+      </NextIntlClientProvider>
     </MainContainer>
   );
 }
