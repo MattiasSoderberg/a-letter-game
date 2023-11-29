6aa00defd2aab54eb7dd38e9b1599c8cde0eb8d4
Mattias Söderberg <mattes.email@gmail.com>
2023-11-29 15:34:02 +0100

    Refactor styles

--- src/app/components/Form/Checkbox.tsx
+++ src/app/components/Form/Checkbox.tsx
@@ -1,20 +1,27 @@
+import { GameSettings } from "@/context/AppContext";
 import React from "react";
+import { Path, UseFormRegister } from "react-hook-form";
+import { MdCheck } from "react-icons/md";
 
-interface Props {
-  name: string;
-  checked: boolean;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
+type Props = {
+  name: Path<GameSettings>;
+  register: UseFormRegister<GameSettings>;
+  registerOptions?: any;
+};
 
-const Checkbox = ({ name, checked, onChange }: Props) => {
+const Checkbox = ({ name, register, registerOptions }: Props) => {
   return (
-    <input
-      type="checkbox"
-      name={name}
-      id={name}
-      checked={checked}
-      onChange={onChange}
-    />
+    <div className="h-full flex items-center relative">
+      <input
+        id={name}
+        type="checkbox"
+        {...register(name, registerOptions && registerOptions[name])}
+        className="appearance-none w-5 h-5 border border-secondLight rounded peer relative bg-lightMain transition-all duration-75 checked:bg-secondLight checked:border-secondMain focus:outline-secondMain"
+      />
+      <div className="w-4 h-4 opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 pointer-events-none transition-opacity duration-75 peer-checked:opacity-100">
+        <MdCheck />
+      </div>
+    </div>
   );
 };
 
