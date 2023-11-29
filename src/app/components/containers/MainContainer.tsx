17332bf974f1f7ead13952a80f778aaecfe0ff21
Mattias Söderberg <mattes.email@gmail.com>
2023-11-29 15:33:38 +0100

    Add overflow y auto

--- src/app/components/containers/MainContainer.tsx
+++ src/app/components/containers/MainContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const MainContainer = ({ children }: Props) => {
   return (
-    <div className="w-full h-full flex flex-col items-center gap-6 px-5 py-8 relative z-20 md:p-20 lg:px-60 xl:px-80 2xl:px-96">
+    <div className="w-full h-full flex flex-col items-center gap-6 px-5 pt-8 pb-24 relative z-20 md:p-20 lg:px-60 xl:px-80 2xl:px-96 overflow-y-auto">
       {children}
     </div>
   );
