464995c86d3d1f3c87c548ed86b3be2b6d50a825
Mattias Söderberg <mattes.email@gmail.com>
2023-12-04 13:32:13 +0100

    Add conditional rendering for all game content

--- src/app/components/Game/Game.tsx
+++ src/app/components/Game/Game.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { useAppContext } from "@/context/AppContext";
-import { TextRegular } from "../Typography";
+import { TextLarge, TextRegular } from "../Typography";
 import { useRouter } from "@/navigation";
 import PlayerCard from "./PlayerCard";
 import LetterGenerator from "./LetterGenerator";
@@ -35,31 +35,37 @@ const Game = () => {
 
   return (
     <div className="w-full h-full flex flex-col gap-10">
-      <div className="flex gap-4">
-        {players.length > 2 ? (
-          <PlayerCardDrawer players={players} />
-        ) : (
-          <div className="w-full flex justify-between">
-            {players.map((player, index) => (
-              <PlayerCard
-                key={player.name + index}
-                name={player.name}
-                points={player.points}
-                index={index}
-              />
-            ))}
+      {players.length > 0 ? (
+        <>
+          <div className="flex gap-4">
+            {players.length > 2 ? (
+              <PlayerCardDrawer players={players} />
+            ) : (
+              <div className="w-full flex justify-between">
+                {players.map((player, index) => (
+                  <PlayerCard
+                    key={player.name + index}
+                    name={player.name}
+                    points={player.points}
+                    index={index}
+                  />
+                ))}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-      <GameTimer
-        isRoundActive={isRoundActive}
-        handleSetRoundIsActive={handleSetRoundIsActive}
-      />
-      <LetterGenerator
-        alphabeth={t("alphabeth")}
-        currentLetter={currentLetter}
-        handleSetCurrentLetter={handleSetCurrentLetter}
-      />
+          <GameTimer
+            isRoundActive={isRoundActive}
+            handleSetRoundIsActive={handleSetRoundIsActive}
+          />
+          <LetterGenerator
+            alphabeth={t("alphabeth")}
+            currentLetter={currentLetter}
+            handleSetCurrentLetter={handleSetCurrentLetter}
+          />
+        </>
+      ) : (
+        <TextLarge>Inget aktivt spel</TextLarge>
+      )}
     </div>
   );
 };
