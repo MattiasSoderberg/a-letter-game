b042e2e58358b74db659b859dc9a4dd1b027d103
Mattias Söderberg <mattes.email@gmail.com>
2023-12-04 11:48:10 +0100

    Refactor players form

--- src/app/components/Game/NewGameForm/NewGameForm.tsx
+++ src/app/components/Game/NewGameForm/NewGameForm.tsx
@@ -1,13 +1,13 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PlayersForm from "./PlayersForm";
-import { GameSettings, useAppContext } from "@/context/AppContext";
+import { GameSettings, Player, useAppContext } from "@/context/AppContext";
 import { useRouter } from "@/navigation";
 import SettingsForm from "./SettingsForm";
 
 const NewGameForm = () => {
   const [isSettingsSubmitted, setIsSettingsSubmitted] = useState(false);
-  const { players, handleSetPlayers, gameSettings, handleSetGameSettings } =
+  const { handleSetPlayers, gameSettings, handleSetGameSettings } =
     useAppContext();
   const router = useRouter();
 
@@ -16,28 +16,17 @@ const NewGameForm = () => {
     setIsSettingsSubmitted(true);
   };
 
-  const onPlayersFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const onPlayersFormSubmit = (players: Player[]) => {
+    handleSetPlayers(players);
     router.push("/game");
   };
-
-  useEffect(() => {
-    if (gameSettings.numberOfPlayers > 0) {
-      const players = [];
-      for (let i = 0; i < gameSettings.numberOfPlayers; i++) {
-        players.push({ name: "", points: 0 });
-      }
-      handleSetPlayers(players);
-    }
-  }, [gameSettings.numberOfPlayers]);
 
   return (
     <>
       {isSettingsSubmitted ? (
         <PlayersForm
-          players={players}
-          handleSetPlayers={handleSetPlayers}
-          onSubmit={onPlayersFormSubmit}
+          numberOfPlayers={gameSettings.numberOfPlayers}
+          onPlayersFormSubmit={onPlayersFormSubmit}
         />
       ) : (
         <SettingsForm
