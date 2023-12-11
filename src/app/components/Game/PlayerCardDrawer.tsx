4a029dd5ef48f405a232a8613f705748d5e787e1
Mattias Söderberg <mattes.email@gmail.com>
2023-12-11 09:12:54 +0100

    Animation when changing category

--- src/app/components/Game/PlayerCardDrawer.tsx
+++ src/app/components/Game/PlayerCardDrawer.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState } from "react";
-import { motion } from "framer-motion";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import ButtonNaked from "../Button/variants/ButtonNaked";
 import {
   MdArrowBackIos,
@@ -11,9 +11,10 @@ import usePlayerCardsDrawer from "@/hooks/usePlayerCardsDrawer";
 import { Player, useAppContext } from "@/context/AppContext";
 import PlayerCard from "./PlayerCard";
 import { GameSettings } from "@/gameConfig";
-import { H2, TextLarge } from "../Typography";
+import { H1 } from "../Typography";
 import ButtonStandard from "../Button/variants/ButtonStandard";
 import { useTranslations } from "next-intl";
+import Divider from "../DrawerMenu/Divider";
 
 interface Props {
   players: Player[];
@@ -31,27 +32,95 @@ const PlayerCardDrawer = ({ players, currentLetter, categories }: Props) => {
   const onClick = () => {
     if (currentCategoryIndex >= categories.length - 1) {
       closePlayerCardDrawer();
-      setCurrentCategoryIndex(0);
     } else {
       setCurrentCategoryIndex((prev) => prev + 1);
     }
   };
 
+  const renderCategory = (text: string) => {
+    const variants = {
+      initial:
+        currentCategoryIndex === 0
+          ? {
+              x: 0,
+              opacity: 0,
+            }
+          : {
+              x: "50%",
+              opacity: 0,
+            },
+      animate: {
+        x: 0,
+        opacity: 1,
+      },
+      exit: {
+        x: "-50%",
+        opacity: 0,
+      },
+    };
+
+    return (
+      <motion.p
+        layout
+        key={`drawer-category-${text}`}
+        variants={variants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{
+          x: { type: "spring", stiffness: "180", damping: "19" },
+          opacity: { duration: 0.2, ease: "easeIn" },
+        }}
+        className="w-full flex justify-center text-3xl text-darkMain"
+      >
+        {text}
+      </motion.p>
+    );
+  };
+
+  useEffect(() => {
+    if (isPlayerCardsDrawerOpen) {
+      setCurrentCategoryIndex(0);
+    }
+  }, [isPlayerCardsDrawerOpen]);
+
   return (
     <motion.div
-      initial={{ x: "-358px" }}
-      animate={isPlayerCardsDrawerOpen ? { x: 0 } : { x: "-358px" }}
+      initial={{ x: "-92%" }}
+      animate={isPlayerCardsDrawerOpen ? { x: 0 } : { x: "-92%" }}
       transition={{ type: "spring", stiffness: 130, damping: 20 }}
-      className={`w-full h-full absolute top-0 left-0 z-50`}
+      className={`w-full h-full absolute top-0 -left-1 z-50`}
     >
-      <div className="w-fit h-full flex items-start relative container-drop-shadow">
-        <div className="w-[358px] h-full flex flex-col gap-6 p-5 bg-lightMain">
-          {currentLetter !== "?" && (
-            <div className="w-full flex flex-col items-center gap-6">
-              <p className="text-3xl text-darkMain">{currentLetter}</p>
-              <motion.p>{categories[currentCategoryIndex].value}</motion.p>
+      <div className="w-full h-full flex items-start relative container-drop-shadow">
+        <div className="w-full h-full flex flex-col gap-10 p-5 bg-lightMain">
+          <div className="w-full flex flex-col gap-6">
+            <div className="w-full flex justify-between">
+              <H1>{t("player_drawer_title")}</H1>
+              {currentLetter !== "?" && (
+                <div className="w-1/5 flex justify-center p-2 rounded-lg border-2 border-firstLight">
+                  <p className="text-2xl text-darkMain">{currentLetter}</p>
+                </div>
+              )}
             </div>
-          )}
+            {currentLetter !== "?" && (
+              <div className="w-full flex flex-col gap-6">
+                <Divider color="darkMain" />
+                <div className="w-full">
+                  <AnimatePresence mode="popLayout">
+                    {renderCategory(categories[currentCategoryIndex].value)}
+                  </AnimatePresence>
+                </div>
+                <div className="w-full flex flex-col">
+                  <ButtonStandard onClick={onClick} size="sm">
+                    {currentCategoryIndex >= categories.length - 1
+                      ? t("player_drawer_button_close")
+                      : t("player_drawer_button_next")}
+                  </ButtonStandard>
+                </div>
+                <Divider color="darkMain" />
+              </div>
+            )}
+          </div>
           <div className="w-full h-fit flex flex-wrap justify-between gap-x-6 gap-y-10">
             {players &&
               players.map((player, index) => (
@@ -62,13 +131,6 @@ const PlayerCardDrawer = ({ players, currentLetter, categories }: Props) => {
                   index={index}
                 />
               ))}
-          </div>
-          <div className="w-full flex flex-col">
-            <ButtonStandard onClick={onClick}>
-              {currentCategoryIndex >= categories.length - 1
-                ? t("player_drawer_button_close")
-                : t("player_drawer_button_next")}
-            </ButtonStandard>
           </div>
         </div>
         <div className="w-[30px] min-w-fit flex pt-5">
