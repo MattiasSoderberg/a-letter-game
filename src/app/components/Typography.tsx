995ccf97d21a1f292fbaf9d3f21efaff5e498de6
Mattias Söderberg <mattes.email@gmail.com>
2023-12-11 09:48:26 +0100

    Add sizes prop

--- src/app/components/Typography.tsx
+++ src/app/components/Typography.tsx
@@ -1,48 +1,83 @@
 import React from "react";
 
+type Size = "lg" | "regular" | "sm" | "xs";
+
 interface Props {
   children: React.ReactNode;
   color?: string;
+  size?: Size;
 }
 
 const globalStyles = {
   color: "darkMain",
 };
 
-export const H1 = ({ children, color = globalStyles.color }: Props) => {
-  const classes = {
-    size: "text-3xl",
+export const H1 = ({
+  children,
+  color = globalStyles.color,
+  size = "regular",
+}: Props) => {
+  const sizes = {
+    lg: "text-4xl",
+    regular: "text-3xl",
+    sm: "text-2xl",
+    xs: "text-xl",
   };
-  return <h1 className={`${classes.size} text-${color}`}>{children}</h1>;
+  return <h1 className={`${sizes[size]} text-${color}`}>{children}</h1>;
 };
 
-export const H2 = ({ children, color = globalStyles.color }: Props) => {
-  const classes = {
-    size: "text-xl",
+export const H2 = ({
+  children,
+  color = globalStyles.color,
+  size = "regular",
+}: Props) => {
+  const sizes = {
+    lg: "text-2xl",
+    regular: "text-xl",
+    sm: "text-lg",
+    xs: "text-base",
   };
-  return <h2 className={`${classes.size} text-${color}`}>{children}</h2>;
+  return <h2 className={`${sizes[size]} text-${color}`}>{children}</h2>;
 };
 
-export const TextLarge = ({ children, color = globalStyles.color }: Props) => {
-  const classes = {
-    size: "text-lg",
+export const TextLarge = ({
+  children,
+  color = globalStyles.color,
+  size = "regular",
+}: Props) => {
+  const sizes = {
+    lg: "text-xl",
+    regular: "text-lg",
+    sm: "text-base",
+    xs: "text-sm",
   };
-  return <p className={`${classes.size} text-${color}`}>{children}</p>;
+  return <p className={`${sizes[size]} text-${color}`}>{children}</p>;
 };
 
 export const TextRegular = ({
   children,
   color = globalStyles.color,
+  size = "regular",
 }: Props) => {
-  const classes = {
-    size: "text-base",
+  const sizes = {
+    lg: "text-lg",
+    regular: "text-base",
+    sm: "text-sm",
+    xs: "text-xs",
   };
-  return <p className={`${classes.size} text-${color}`}>{children}</p>;
+  return <p className={`${sizes[size]} text-${color}`}>{children}</p>;
 };
 
-export const TextSmall = ({ children, color = globalStyles.color }: Props) => {
-  const classes = {
-    size: "text-sm",
+export const TextSmall = ({
+  children,
+  color = globalStyles.color,
+  size = "regular",
+}: Props) => {
+  const sizes = {
+    lg: "text-base",
+    regular: "text-sm",
+    sm: "text-xs",
+    xs: "text-[10px]",
   };
-  return <p className={`${classes.size} text-${color}`}>{children}</p>;
+  return <p className={`${sizes[size]} text-${color}`}>{children}</p>;
 };
