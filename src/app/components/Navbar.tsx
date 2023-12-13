1f4ad422df8d582feb20d26edde6168d4d70dc40
Mattias Söderberg <mattes.email@gmail.com>
2023-12-13 12:05:48 +0100

    Fix close menu when navigating to home

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,12 +1,15 @@
+"use client";
 import React from "react";
 import Logo from "./SVG/Logo";
 import ButtonMenu from "./Button/variants/ButtonMenu";
-import Link from "next/link";
+import { Link } from "@/navigation";
+import useMenu from "@/hooks/useMenu";
 
 const Navbar = () => {
+  const { closeMenu } = useMenu();
   return (
     <div className="w-full h-navHeight flex justify-between bg-lightMain relative z-30 p-5 md:px-10">
-      <Link href="/">
+      <Link href="/" onClick={closeMenu}>
         <Logo />
       </Link>
       <ButtonMenu />
